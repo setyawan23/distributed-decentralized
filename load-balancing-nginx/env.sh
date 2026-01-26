4afc3e79e7c0655a57e27664035b155839503a6e
setyawan23 <145740466+setyawan23@users.noreply.github.com>
2026-01-27 00:07:56 +0700

    Delete load-balancing-nginx/env.sh

--- load-balancing-nginx/env.sh
+++
@@ -1,4 +0,0 @@
-alias dcu="sudo docker-compose up --build -d --scale bs_app=2"
-alias dcd="sudo docker compose down"
-alias dps="docker ps --format '{{.Image}} {{.Names}}'"
-alias dcln="docker logs nginx"
